c6163871fdd3a2802246aabb3af2740b8ab723ee
RiskyNabil213 <risky_pahlevi_33rpl@student.smktelkom-mlg.sch.id>
2025-10-15 13:08:15 +0700

    Fix App import path/casing

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./app"; // resolves to src/app/index.jsx
 import "./styles/tailwind.css";
 import "./styles/index.css";
 
