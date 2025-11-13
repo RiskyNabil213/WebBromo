fd18f59245fa68a8921313d41c6700411111f397
RiskyNabil213 <risky_pahlevi_33rpl@student.smktelkom-mlg.sch.id>
2025-11-13 11:41:54 +0700

    Fix import path and browserslist

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Routes from "./Routes";
 
-function App() { /* ... */ }
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./app.jsx"; // matches your actual file: app.jsx (lowercase)
 import "./styles/tailwind.css";
 import "./styles/index.css";
 
